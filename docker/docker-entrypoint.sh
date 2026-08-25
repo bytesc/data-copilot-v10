ee358e12e383d8dc0bb6d3b70194b9fbd8a58284
rcy <21010078@mail.ecust.edu.cn>
2026-08-25 14:26:14 +0800

    docker

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 set -e
 
-echo "Waiting for MySQL to be ready..."
+echo "Reading database config from config/config.yaml..."
+DB_URL=$(python -c "
+import yaml
+with open('/app/config/config.yaml') as f:
+    cfg = yaml.safe_load(f)
+print(cfg['mysql'])
+")
+
+echo "Waiting for MySQL to be ready: $DB_URL"
 until python -c "
 import time
 import sqlalchemy
 from sqlalchemy import text
+url = '$DB_URL'
 while True:
     try:
-        engine = sqlalchemy.create_engine('mysql+pymysql://root:123456@mysql:3306/2026start_v3')
+        engine = sqlalchemy.create_engine(url)
         with engine.connect() as conn:
             conn.execute(text('SELECT 1'))
         break
